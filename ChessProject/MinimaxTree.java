dc2b85fa26b02bfa8cfa2c58c7a6703da534e52a
Kristoffer Klokker <kristoffer@kklokker.com>
2022-01-12 18:16:19 +0100

    Chess changes optimization and not using list and better tree search for best average move

--- ChessProject/MinimaxTree.java
+++ ChessProject/MinimaxTree.java
@@ -3,39 +3,31 @@
  */
 public class MinimaxTree {
 	private Board board;
-	private nodeList nodes;
+	private MinimaxTree[] nodes;
 	private boolean isWhite;
 
 	/**
-	 * Creates a tree with childs which is possible boards from the current board
-	 * @param board The board the trees root is at
-	 * @param depth The deoth of the tree 
-	 * @param isWhite if the tress root is white
-	 */
+  	* Construct this tree
+  	* @param board The board which the tree is created from
+  	* @param depth The depth of the tree where 0 is no children
+ 	* @param isWhite The color of the current player
+ 	*/
 	public MinimaxTree(Board board, int depth, boolean isWhite) {
 		this.board = board;
 		this.isWhite = isWhite;
-		nodes = new nodeList();
-		if(depth>0)
-			addNodes(this, depth);
-	}
-
-	/**
-	 * Creates children to a root from all possible moves
-	 * @param tree The root whihc children is added to
-	 * @param depth The remaining depth of the tree
-	 */
-	private void addNodes(MinimaxTree tree, int depth) {
-		Move[] moves = tree.board.legalMoves();
-		for(Move move: moves) {
-			Board copy = tree.board.copy();
-			if(copy.move(move))
-				copy.changeType(move.to(), 'Q');
-			MinimaxTree node = new MinimaxTree(copy, depth-1, isWhite);
-			tree.nodes.add(node);
+		nodes = new MinimaxTree[0];
+		if(depth>0) {
+			Move[] moves = board.legalMoves();
+			nodes = new MinimaxTree[moves.length];
+			for(int i = 0; i < moves.length; i++) {
+				Board copy = board.copy();
+				copy.move(moves[i]);
+				MinimaxTree node = new MinimaxTree(copy, depth-1, isWhite);
+				nodes[i] = node;
+			}
 		}
 	}
-	
+
 	/**
 	 * Gives a value of the board according to values from
 	 * https://www.chess.com/terms/chess-piece-value
@@ -75,9 +67,9 @@ public class MinimaxTree {
 	public Move next() {
 		int score = Integer.MIN_VALUE;
 		int index = 0;
-		for(int i = 0; i < nodes.length(); i++)
-			if(score < nodes.At(i).nodeScore()) {
-				score = nodes.At(i).nodeScore();
+		for(int i = 0; i < nodes.length; i++)
+			if(score < nodes[i].nodeScore()) {
+				score = nodes[i].nodeScore();
 				index = i; 
 			}
 		return board.legalMoves()[index];
@@ -87,66 +79,15 @@ public class MinimaxTree {
 	 * @return The best score of the node and its children
 	 */
 	private int nodeScore() {
-		if(nodes.length() == 0)
+		if(nodes.length == 0)
 			return heuristic(board,isWhite);
-		int score = heuristic(nodes.At(0).board, isWhite);
-		for(int i = 1; i < nodes.length(); i++) {
-			int childScore = heuristic(nodes.At(i).board, isWhite);
-			score = (score < childScore) ? childScore : score;
+		int score = 0;
+		for(int i = 0; i < nodes.length; i++) {
+			int childScore = heuristic(nodes[i].board, isWhite);
+			score = score + childScore;
 		}
+		score = score / nodes.length;
 		return score;
 	}
 
-	/**
-	 * Class for the list of children to a roow
-	 */
-	private class nodeList {
-		private int index;
-		private MinimaxTree[] nodes;
-
-		/**
-		 * Creates a list of trees with length 10
-		 */
-		private nodeList() {
-			index = 0;
-			nodes = new MinimaxTree[10];
-		}
-
-		/**
-		 * Adds an element to this list
-		 * @param node The node which should be added to the list
-		 */
-		private void add(MinimaxTree node) {
-			if(index == nodes.length)
-				expand();
-			nodes[index] = node;
-			index++;
-		}
-
-		/**
-		 * Expands the list to the double length
-		 */
-		private void expand() {
-			MinimaxTree[] newNodes = new MinimaxTree[nodes.length * 2];
-			for(int i = 0; i < index; i++)
-				newNodes[i] = nodes[i];
-			nodes = newNodes;
-		}
-
-		/**
-		 * @return the length of the list
-		 */
-		public int length() {
-			return index;
-		}
-
-		/**
-		 * @param the index of the wanted node
-		 * @return the node at index i
-		 */
-		public MinimaxTree At(int i) {
-			return nodes[i];
-		}
-
-	}
 }
